51c55b71544d4cb5a9646cc70cac506fa0b94cd5
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-09-25 17:43:16 +0200

    feat: bridge

---
+++ apps/bridge/master.py
@@ -0,0 +1,27 @@
+import RPi.GPIO as GPIO
+import time
+
+DATA_PIN = 18
+CLK_PIN = 24
+
+# Setup GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DATA_PIN, GPIO.OUT)
+GPIO.setup(CLK_PIN, GPIO.OUT)
+
+
+def send_data(data):
+    for bit in "{:08b}".format(data):  # Send 8-bit data
+        GPIO.output(DATA_PIN, int(bit))
+        GPIO.output(CLK_PIN, GPIO.HIGH)
+        time.sleep(0.01)
+        GPIO.output(CLK_PIN, GPIO.LOW)
+        time.sleep(0.01)
+
+
+try:
+    while True:
+        send_data(0xA5)  # Example: Send 0xA5
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
---
+++ apps/bridge/slave.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+
+DATA_PIN = 18
+CLK_PIN = 24
+
+# Setup GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(DATA_PIN, GPIO.IN)
+GPIO.setup(CLK_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+def receive_data():
+    data = ""
+    while len(data) < 8:
+        if GPIO.input(CLK_PIN) == GPIO.HIGH:
+            data += str(GPIO.input(DATA_PIN))
+            while GPIO.input(CLK_PIN) == GPIO.HIGH:
+                pass  # Wait for clock to go low to prepare for next bit
+    return int(data, 2)
+
+
+try:
+    while True:
+        received = receive_data()
+        print(f"Received: {received:02X}")
+        time.sleep(1)  # Adjust sleep time if necessary
+except KeyboardInterrupt:
+    GPIO.cleanup()
