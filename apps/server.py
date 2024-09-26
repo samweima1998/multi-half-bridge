2205c5f7de43bad0df3f52043591e74faf4db397
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-09-26 17:46:56 +0200

    feat: server

---
+++ apps/server.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import subprocess
+import uvicorn
+
+
+app = FastAPI()
+
+
+class Command(BaseModel):
+    cs_pin: str
+    args: str
+
+
+@app.post("/execute")
+def execute_command(command: Command):
+    """
+    Endpoint to execute a given command via command line.
+    """
+    command_args = ["./build/control", command.cs_pin + command.args]
+
+    try:
+        subprocess.run(command_args, check=True)
+        return {"status": True}
+    except subprocess.CalledProcessError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+if __name__ == "__main__":
+    # Use uvicorn to run the application.
+    # By default, it will run on http://127.0.0.1:8000
+    # The command `reload=True` enables the server to reload on code changes.
+    uvicorn.run(app, host="0.0.0.0", port=7070, log_level="info")
